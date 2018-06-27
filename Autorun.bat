d5a69978dd6f2ebfefe7a7db26d1eddaad80afd3
Osama Hassanein <player1os@gmail.com>
2018-06-27 16:51:54 +0200

    Simplify the autorun script

--- Autorun.bat
+++ Autorun.bat
@@ -1,8 +1,5 @@
 @ECHO OFF
 
-:: Switch to the deploy directory.
-PUSHD \\kedata\Data1\B2B_Business_Inteligence\Osama Hassanein\ExcelMailSender
-
 :: Set the autorun parameters.
 SET APP_IS_AUTORUN_MODE=TRUE
 SET APP_INPUT_TABLE_FILE_PATH=H:\INPUT_TABLE.xlsx
@@ -10,6 +7,3 @@ SET APP_BODY_TEMPLATE_FILE_PATH=H:\BODY_TEMPLATE.txt
 
 :: Run the main project workbook.
 CALL "MailSender.xlsm"
-
-:: Return to the original working directory.
-POPD
